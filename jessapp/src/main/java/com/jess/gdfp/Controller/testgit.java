a4efd3f77845ce9946e0ee8d76321d6302e83161
jtan <tan_jiajing@yahoo,com>
2020-08-31 12:26:22 +0200

    update 12:24 31.08.2020

--- jessapp/src/main/java/com/jess/gdfp/Controller/testgit.java
+++ jessapp/src/main/java/com/jess/gdfp/Controller/testgit.java
@@ -3,5 +3,7 @@ package com.jess.gdfp.Controller;
 public class testgit {
     public  testgit(){
         System.out.println("hshsh");
-        System.out.println("hallo git" ); }
+        System.out.println("hallo git" );
+        System.out.println("hallo ich bin jj");
+    }
 }
