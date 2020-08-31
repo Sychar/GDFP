aff42e8f0c412b2f6ef08e3112b1f6ab05e306ee
saljend <sakhr.aljumdi@gmail.com>
2020-08-31 12:19:21 +0200

    31.08.2020 12:12

--- jessapp/src/main/java/com/jess/gdfp/Controller/testgit.java
+++ jessapp/src/main/java/com/jess/gdfp/Controller/testgit.java
@@ -2,6 +2,6 @@ package com.jess.gdfp.Controller;
 
 public class testgit {
     public  testgit(){
-        System.out.println("hallo git" );
-    }
+        System.out.println("hshsh");
+        System.out.println("hallo git" ); }
 }
